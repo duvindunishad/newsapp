32143e32bfebc43f2c18f2c46fdfaed12df6715c
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-10-14 12:54:21 +0530

    mobile responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,10 +8,17 @@ import SearchForm from './SearchForm';
 
 export default function Header() {
     const [open, setOpen] = useState(false);
+    const [on, setOn] = useState(false);
     const handleFormOpen = (e: Event | any) =>{
         e.preventDefault();
         setOpen(!open);
-    }
+    };
+
+    const handleToggleMenu = () =>{
+        setOn(!on);
+        const body: HTMLElement | any = document.querySelector('body');
+        body.classList.toggle('mobile-nav-active');
+    };
   return (
     <header id='header' className='header d-flex align-items-center fixed-top'>
         <div className="container-fluid container-xl d-flex align-items-center justify-content-between" >
@@ -25,6 +32,13 @@ export default function Header() {
                 <a className='mx-2 js-search-open' onClick={handleFormOpen}>
                     <span className='bi-search'></span>
                 </a>
+                {
+                    on ? (
+                        <i className='bi bi-x mobile-nav-toggle' onClick={handleToggleMenu}></i>
+                    ):(
+                        <i className='bi bi-list mobile-nav-toggle' onClick={handleToggleMenu}></i>
+                    )
+                }
                 <SearchForm active={open} formOpen={handleFormOpen}/>
             </div>
         </div>
